f8d686079b27c23509d964900eb50a1db6d3ffea
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-27 11:35:13 -0600

    Add StudentController class and get method

---
+++ src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.israel.spring_boot_restapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/institution")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getStudents(){
+        return studentService.getStudents();
+    }
+
+}
