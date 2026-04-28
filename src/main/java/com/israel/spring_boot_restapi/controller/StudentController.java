b6402c77ac8fdf9c82b497c04c5921509eb424f8
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-28 08:45:41 -0600

    Import StudentService class

--- src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
+++ src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.israel.spring_boot_restapi.controller;
 
+import com.israel.spring_boot_restapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
