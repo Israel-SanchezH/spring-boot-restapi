18772da3e6f52b9940a7d01df7e529d9f1118eba
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-28 09:22:57 -0600

    Import Student, ResponseEntity, List Classes

--- src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
+++ src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.israel.spring_boot_restapi.controller;
 
+import com.israel.spring_boot_restapi.entity.Student;
 import com.israel.spring_boot_restapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/institution")
