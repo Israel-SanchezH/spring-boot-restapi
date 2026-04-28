06fcae5b8c1d46a72b7fab5deea27374121a7565
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-28 10:00:36 -0600

    Import PathVariable and Optional Classes, Add /api to @RequestMapping and Define /students/{id} endpoint

--- src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
+++ src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
@@ -5,13 +5,16 @@ import com.israel.spring_boot_restapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/institution")
+@RequestMapping("/api/v1/institution")
 public class StudentController {
 
     @Autowired
@@ -20,6 +23,11 @@ public class StudentController {
     @GetMapping("/students")
     public ResponseEntity<List<Student>> getStudents(){
         return studentService.getStudents();
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Optional<Student>> getStudent(@PathVariable int id){
+        return studentService.getStudent(id);
     }
 
 }
