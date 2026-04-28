062c2dce893921af4fbc388050bd353ad78f10ec
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-28 10:02:11 -0600

    Import Optional class and implement getStudent method

--- src/main/java/com/israel/spring_boot_restapi/service/StudentService.java
+++ src/main/java/com/israel/spring_boot_restapi/service/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,4 +19,7 @@ public class StudentService {
         return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
     }
 
+    public ResponseEntity<Optional<Student>> getStudent(int id) {
+        return new ResponseEntity<>(studentRepository.findById(id), HttpStatus.OK);
+    }
 }
