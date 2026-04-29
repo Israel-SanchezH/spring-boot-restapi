9744a99f7755ad25ef3e1c412d51449092572ff4
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-29 13:19:30 -0600

    Remove GeneratedValue

--- src/main/java/com/israel/spring_boot_restapi/entity/Student.java
+++ src/main/java/com/israel/spring_boot_restapi/entity/Student.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstname;
     private String lastname;
