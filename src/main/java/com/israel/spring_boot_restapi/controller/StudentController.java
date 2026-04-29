f13c4be7f4d50882e7ef1576f36eb08b21e65c86
Israel-SanchezH <ish.comunicaciones@gmail.com>
2026-04-29 13:17:52 -0600

    Import HttpStatus and annotation.* to eliminate redundant imports, Add addStudent method for post request

--- src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
+++ src/main/java/com/israel/spring_boot_restapi/controller/StudentController.java
@@ -3,11 +3,9 @@ package com.israel.spring_boot_restapi.controller;
 import com.israel.spring_boot_restapi.entity.Student;
 import com.israel.spring_boot_restapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -30,4 +28,8 @@ public class StudentController {
         return studentService.getStudent(id);
     }
 
+    @PostMapping("/students")
+    public ResponseEntity<Student> addStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
 }
